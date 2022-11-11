a2d2eb66d49c8de04ff4f3083c924fa219b9cbba
Levminer <levminer@levminer.com>
2022-11-11 15:40:41 +0100

    Navigation

--- Cores/MainWindow.xaml.cs
+++ Cores/MainWindow.xaml.cs
@@ -1,32 +1,44 @@
-﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+﻿using cores;
+using Microsoft.UI.Xaml;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace Cores {
-	/// <summary>
-	/// An empty window that can be used on its own or navigated to within a Frame.
-	/// </summary>
-	public sealed partial class MainWindow : Window {
-		public MainWindow() {
-			this.InitializeComponent();
-		}
+namespace Cores;
+/// <summary>
+/// An empty window that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class MainWindow : Window {
+	public MainWindow() {
+		InitializeComponent();
 
-		private void myButton_Click(object sender, RoutedEventArgs e) {
-			myButton.Content = "Clicked";
+		// Set titlebar
+		SetTitleBar(AppTitleBar);
+		ExtendsContentIntoTitleBar = true;
+
+		// Set Mica
+		var mica = new Mica(this);
+		mica.TrySetSystemBackdrop();
+	}
+
+	private void mainNavigation_SelectionChanged(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewSelectionChangedEventArgs args) {
+		if (args.IsSettingsSelected) {
+			// contentFrame5.Navigate(typeof(SampleSettingsPage));
+		} else {
+			var selectedItem = (Microsoft.UI.Xaml.Controls.NavigationViewItem)args.SelectedItem;
+			var selectedItemTag = ((string)selectedItem.Tag);
+			sender.Header = selectedItem.Content;
+
+			var pageName = $"Cores.Views.{selectedItemTag}";
+			Type pageType = Type.GetType(pageName);
+			mainContent.Navigate(pageType);
 		}
+	}
+
+	private void mainNavigation_Loaded(object sender, RoutedEventArgs e) {
+		var pageName = "Cores.Views.CPUView";
+		Type pageType = Type.GetType(pageName);
+		mainContent.Navigate(pageType);
 	}
 }
