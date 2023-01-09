7e76a5a12623a1f0943d13051adf8fae13d820af
Lőrik Levente <levminer@levminer.com>
2023-01-09 17:20:57 +0100

    Update packages, disable svelte a11y warnings

--- interface/vite.config.ts
+++ interface/vite.config.ts
@@ -3,7 +3,15 @@ import { svelte } from "@sveltejs/vite-plugin-svelte"
 import sveltePreprocess from "svelte-preprocess"
 
 export default defineConfig({
-	plugins: [svelte({ preprocess: sveltePreprocess() })],
+	plugins: [
+		svelte({
+			preprocess: sveltePreprocess(),
+			onwarn: (warning, handler) => {
+				if (warning.code.startsWith("a11y-")) return
+				handler(warning)
+			},
+		}),
+	],
 	base: "",
 	server: {
 		port: 3000,
