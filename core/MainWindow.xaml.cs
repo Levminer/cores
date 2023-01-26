7251577e213e29231a35ab6ef1a7b4fe4d91e84b
Lőrik Levente <levminer@levminer.com>
2023-01-26 15:20:46 +0100

    Update WA SDK

--- core/MainWindow.xaml.cs
+++ core/MainWindow.xaml.cs
@@ -123,7 +123,7 @@ public sealed partial class MainWindow : Window {
 
 		App.GlobalHardwareInfo.API.System.OS.WebView = webView.CoreWebView2.Environment.BrowserVersionString;
 		App.GlobalHardwareInfo.API.System.OS.App = $"{appVersion.Major}.{appVersion.Minor}.{appVersion.Build}";
-		App.GlobalHardwareInfo.API.System.OS.Runtime = "1.2.221209.1";
+		App.GlobalHardwareInfo.API.System.OS.Runtime = "1.2.230118.102";
 
 		var JSON = JsonSerializer.Serialize(App.GlobalHardwareInfo.API, App.SerializerOptions);
 
