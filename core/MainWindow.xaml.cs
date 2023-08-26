563cfd472fb1730d5fc972e794736e1453805229
Lőrik Levente <levminer@levminer.com>
2023-08-26 15:25:14 +0200

    Update packages

--- core/MainWindow.xaml.cs
+++ core/MainWindow.xaml.cs
@@ -136,7 +136,7 @@ public sealed partial class MainWindow : Window {
 
 		App.GlobalHardwareInfo.API.System.OS.WebView = webView.CoreWebView2.Environment.BrowserVersionString;
 		App.GlobalHardwareInfo.API.System.OS.App = $"{appVersion.Major}.{appVersion.Minor}.{appVersion.Build}";
-		App.GlobalHardwareInfo.API.System.OS.Runtime = "1.3.230502000";
+		App.GlobalHardwareInfo.API.System.OS.Runtime = "1.3.230724000";
 
 
 		var message = new Message() {
