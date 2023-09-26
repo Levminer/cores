05c2d6b0120878d4dbba4e63d3548fcddaa72b2f
Lőrik Levente <levminer@levminer.com>
2023-09-26 17:08:20 +0200

    Update packages

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,14 +1,11 @@
 module.exports = {
-	trailingComma: "es5",
+	trailingComma: "all",
 	tabWidth: 4,
-	printWidth: 1000,
+	printWidth: 150,
 	useTabs: true,
 	singleQuote: false,
 	semi: false,
 	svelteSortOrder: "options-markup-scripts-styles",
-	"plugins": [
-		"prettier-plugin-svelte",
-		"prettier-plugin-tailwindcss"
-	  ],
-	  "pluginSearchDirs": false
+	plugins: ["prettier-plugin-svelte", "prettier-plugin-tailwindcss"],
+	pluginSearchDirs: false,
 }
