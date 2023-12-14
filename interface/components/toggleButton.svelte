3e9ea971f1a3f6e25ad16747976f8495246aba95
Lőrik Levente <levminer@levminer.com>
2023-12-14 17:56:29 +0100

    Create toggleButton.svelte

---
+++ interface/components/toggleButton.svelte
@@ -0,0 +1,11 @@
+<button on:click class="mx-auto flex flex-1">
+	<a
+		class="transparent-800 group inline-flex flex-shrink-0 flex-grow flex-col items-center justify-center rounded-full p-1 px-5 text-gray-200 duration-200 ease-in-out"
+	>
+		<p class="text-xl">{selected ? "60m" : "60s"}</p>
+	</a>
+</button>
+
+<script lang="ts">
+	export let selected = false
+</script>
