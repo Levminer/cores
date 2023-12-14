1482d9f92df32cff957f10e90fed903abf17f694
Lőrik Levente <levminer@levminer.com>
2023-12-14 17:56:56 +0100

    Update cpu page

--- interface/pages/cpu.svelte
+++ interface/pages/cpu.svelte
@@ -1,12 +1,12 @@
-<div class="transparent-900 m-10 mx-auto w-11/12 rounded-xl sm:m-3 sm:w-full">
-	<div class="mx-10 flex gap-5 pb-10 pt-10 sm:flex-wrap">
+<div class="transparent-900 m-10 mx-auto w-11/12 rounded-xl sm:w-full">
+	<div class="mx-10 flex gap-5 pb-10 pt-10 sm:mx-3 sm:flex-wrap">
 		<div class="flex w-3/5 flex-col justify-start gap-5 sm:w-full">
 			<!-- cpu info -->
-			<div class="transparent-800 rounded-xl p-10 text-left">
-				<div class="mb-5 flex items-baseline gap-3">
-					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-cpu" viewBox="0 0 16 16">
-						<path d="M5 0a.5.5 0 0 1 .5.5V2h1V.5a.5.5 0 0 1 1 0V2h1V.5a.5.5 0 0 1 1 0V2h1V.5a.5.5 0 0 1 1 0V2A2.5 2.5 0 0 1 14 4.5h1.5a.5.5 0 0 1 0 1H14v1h1.5a.5.5 0 0 1 0 1H14v1h1.5a.5.5 0 0 1 0 1H14v1h1.5a.5.5 0 0 1 0 1H14a2.5 2.5 0 0 1-2.5 2.5v1.5a.5.5 0 0 1-1 0V14h-1v1.5a.5.5 0 0 1-1 0V14h-1v1.5a.5.5 0 0 1-1 0V14h-1v1.5a.5.5 0 0 1-1 0V14A2.5 2.5 0 0 1 2 11.5H.5a.5.5 0 0 1 0-1H2v-1H.5a.5.5 0 0 1 0-1H2v-1H.5a.5.5 0 0 1 0-1H2v-1H.5a.5.5 0 0 1 0-1H2A2.5 2.5 0 0 1 4.5 2V.5A.5.5 0 0 1 5 0zm-.5 3A1.5 1.5 0 0 0 3 4.5v7A1.5 1.5 0 0 0 4.5 13h7a1.5 1.5 0 0 0 1.5-1.5v-7A1.5 1.5 0 0 0 11.5 3h-7zM5 6.5A1.5 1.5 0 0 1 6.5 5h3A1.5 1.5 0 0 1 11 6.5v3A1.5 1.5 0 0 1 9.5 11h-3A1.5 1.5 0 0 1 5 9.5v-3zM6.5 6a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3z" />
-					</svg>
+			<div class="transparent-800 rounded-xl p-8 sm:p-4">
+				<div class="mb-5 flex items-center gap-3">
+					<div class="transparent-900 flex aspect-square items-center justify-center rounded-lg p-3 sm:p-2">
+						<Cpu />
+					</div>
 					<h2>CPU Info</h2>
 				</div>
 				<div class="select-text">
@@ -19,92 +19,134 @@
 			</div>
 
 			<!-- cpu temp -->
-			<div class="transparent-800 rounded-xl p-10 text-left">
+			<div class="transparent-800 rounded-xl p-8 sm:p-4">
 				<div class="mb-5 flex items-baseline justify-between">
-					<div class="flex items-baseline gap-3">
-						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 4v10.54a4 4 0 1 1-4 0V4a2 2 0 0 1 4 0Z" /></svg>
+					<div class="mb-5 flex items-center gap-3">
+						<div class="transparent-900 flex aspect-square items-center justify-center rounded-lg p-3 sm:p-2">
+							<Thermometer />
+						</div>
 						<h2>Average CPU Temperature</h2>
 					</div>
 					<div>
-						<h5 class="cursor-pointer" on:click={() => (minutes = !minutes)}>{minutes ? "Last 60 minutes" : "Last 60 seconds"}</h5>
+						<ToggleButton selected={minutes} on:click={() => (minutes = !minutes)} />
 					</div>
 				</div>
 
-				<div>
-					<MultiLineChart statistics={minutes ? $hardwareStatistics.minutes.map((value) => value.cpu.temperature) : $hardwareStatistics.seconds.map((value) => value.cpu.temperature)} time={minutes ? "m" : "s"} />
+				<div class="cursor-crosshair">
+					<MultiLineChart
+						statistics={minutes
+							? $hardwareStatistics.minutes.map((value) => value.cpu.temperature)
+							: $hardwareStatistics.seconds.map((value) => value.cpu.temperature)}
+						time={minutes ? "m" : "s"}
+					/>
 				</div>
 			</div>
 
 			<!-- clock speed -->
-			<div class="transparent-800 rounded-xl p-10 text-left">
+			<div class="transparent-800 rounded-xl p-8 sm:p-4">
 				<div class="mb-5 flex items-baseline justify-between">
-					<div class="flex items-baseline gap-3">
-						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10" /><polyline points="12 6 12 12 16 14" /></svg>
+					<div class="mb-5 flex items-center gap-3">
+						<div class="transparent-900 flex aspect-square items-center justify-center rounded-lg p-3 sm:p-2">
+							<Clock />
+						</div>
 						<h2>Average Clock speed</h2>
 					</div>
 					<div>
-						<h5 class="cursor-pointer" on:click={() => (minutes = !minutes)}>{minutes ? "Last 60 minutes" : "Last 60 seconds"}</h5>
+						<ToggleButton selected={minutes} on:click={() => (minutes = !minutes)} />
 					</div>
 				</div>
 
 				<div>
-					<MultiLineChart name="Clock Speed" unit={"Mhz"} statistics={minutes ? $hardwareStatistics.minutes.map((value) => value.cpu.clock) : $hardwareStatistics.seconds.map((value) => value.cpu.clock)} time={minutes ? "m" : "s"} />
+					<MultiLineChart
+						name="Clock Speed"
+						unit={"Mhz"}
+						statistics={minutes
+							? $hardwareStatistics.minutes.map((value) => value.cpu.clock)
+							: $hardwareStatistics.seconds.map((value) => value.cpu.clock)}
+						time={minutes ? "m" : "s"}
+					/>
 				</div>
 			</div>
 		</div>
 
 		<div class="flex w-2/5 flex-col justify-start gap-5 sm:w-full">
 			<!-- cpu power usage -->
-			<div class="transparent-800 rounded-xl p-10 text-left">
+			<div class="transparent-800 rounded-xl p-8 sm:p-4">
 				<div class="mb-5 flex items-baseline justify-between">
-					<div class="flex items-baseline gap-3">
-						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 22v-5" /><path d="M9 7V2" /><path d="M15 7V2" /><path d="M6 13V8h12v5a4 4 0 0 1-4 4h-4a4 4 0 0 1-4-4Z" /></svg>
+					<div class="mb-5 flex items-center gap-3">
+						<div class="transparent-900 flex aspect-square items-center justify-center rounded-lg p-3 sm:p-2">
+							<Plug />
+						</div>
 						<h2>CPU Power Usage</h2>
 					</div>
 					<div>
-						<h5 class="cursor-pointer" on:click={() => (minutes = !minutes)}>{minutes ? "Last 60m" : "Last 60s"}</h5>
+						<ToggleButton selected={minutes} on:click={() => (minutes = !minutes)} />
 					</div>
 				</div>
 
 				<div>
-					<LineChart statistics={minutes ? $hardwareStatistics.minutes.map((value) => value.cpu.power) : $hardwareStatistics.seconds.map((value) => value.cpu.power)} type={"Power usage"} unit={" W"} color={"#ffd60a"} time={minutes ? "m" : "s"} />
+					<LineChart
+						statistics={minutes
+							? $hardwareStatistics.minutes.map((value) => value.cpu.power)
+							: $hardwareStatistics.seconds.map((value) => value.cpu.power)}
+						type={"Power usage"}
+						unit={" W"}
+						color={"#ffd60a"}
+						time={minutes ? "m" : "s"}
+					/>
 				</div>
 			</div>
 
 			<!-- cpu load -->
-			<div class="transparent-800 rounded-xl p-10 text-left">
+			<div class="transparent-800 rounded-xl p-8 sm:p-4">
 				<div class="mb-5 flex items-baseline justify-between">
-					<div class="flex items-baseline gap-3">
-						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
-							<path d="M8 4a.5.5 0 0 1 .5.5V6a.5.5 0 0 1-1 0V4.5A.5.5 0 0 1 8 4zM3.732 5.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 10a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 10zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 9.31a.91.91 0 1 0 1.302 1.258l3.434-4.297a.389.389 0 0 0-.029-.518z" />
-							<path fill-rule="evenodd" d="M0 10a8 8 0 1 1 15.547 2.661c-.442 1.253-1.845 1.602-2.932 1.25C11.309 13.488 9.475 13 8 13c-1.474 0-3.31.488-4.615.911-1.087.352-2.49.003-2.932-1.25A7.988 7.988 0 0 1 0 10zm8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.397 12.533 6.358 12 8 12s3.604.532 4.923.96c.757.245 1.477-.056 1.68-.631A7 7 0 0 0 8 3z" />
-						</svg>
+					<div class="mb-5 flex items-center gap-3">
+						<div class="transparent-900 flex aspect-square items-center justify-center rounded-lg p-3 sm:p-2">
+							<Gauge />
+						</div>
 						<h2>Average CPU Load</h2>
 					</div>
 					<div>
-						<h5 class="cursor-pointer" on:click={() => (minutes = !minutes)}>{minutes ? "Last 60m" : "Last 60s"}</h5>
+						<ToggleButton selected={minutes} on:click={() => (minutes = !minutes)} />
 					</div>
 				</div>
 
 				<div>
-					<UsageChart statistics={minutes ? $hardwareStatistics.minutes.map((value) => value.cpu.load) : $hardwareStatistics.seconds.map((value) => value.cpu.load)} type={"Load"} unit={"%"} time={minutes ? "m" : "s"} />
+					<UsageChart
+						statistics={minutes
+							? $hardwareStatistics.minutes.map((value) => value.cpu.load)
+							: $hardwareStatistics.seconds.map((value) => value.cpu.load)}
+						type={"Load"}
+						unit={"%"}
+						time={minutes ? "m" : "s"}
+					/>
 				</div>
 			</div>
 
 			<!-- cpu voltage -->
-			<div class="transparent-800 rounded-xl p-10 text-left">
+			<div class="transparent-800 rounded-xl p-8 sm:p-4">
 				<div class="mb-5 flex items-baseline justify-between">
-					<div class="flex items-baseline gap-3">
-						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2" /></svg>
+					<div class="mb-5 flex items-center gap-3">
+						<div class="transparent-900 flex aspect-square items-center justify-center rounded-lg p-3 sm:p-2">
+							<Zap />
+						</div>
 						<h2>CPU Voltage</h2>
 					</div>
 					<div>
-						<h5 class="cursor-pointer" on:click={() => (minutes = !minutes)}>{minutes ? "Last 60m" : "Last 60s"}</h5>
+						<ToggleButton selected={minutes} on:click={() => (minutes = !minutes)} />
 					</div>
 				</div>
 
 				<div>
-					<LineChart statistics={minutes ? $hardwareStatistics.minutes.map((value) => value.cpu.voltage) : $hardwareStatistics.seconds.map((value) => value.cpu.voltage)} type={"Voltage"} unit={" V"} color={"#ffd60a"} time={minutes ? "m" : "s"} />
+					<LineChart
+						statistics={minutes
+							? $hardwareStatistics.minutes.map((value) => value.cpu.voltage)
+							: $hardwareStatistics.seconds.map((value) => value.cpu.voltage)}
+						type={"Voltage"}
+						unit={" V"}
+						color={"#ffd60a"}
+						time={minutes ? "m" : "s"}
+					/>
 				</div>
 			</div>
 		</div>
@@ -117,6 +159,8 @@
 	import UsageChart from "../components/usageChart.svelte"
 	import { hardwareStatistics } from "../stores/hardwareStatistics"
 	import { hardwareInfo } from "../stores/hardwareInfo"
+	import { Clock, Cpu, Gauge, Plug, Thermometer, Zap } from "lucide-svelte"
+	import ToggleButton from "@components/toggleButton.svelte"
 
 	let minutes = false
 </script>
