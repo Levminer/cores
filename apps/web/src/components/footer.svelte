f1d6b4a42cbae1a0339e717ca0ceb02afddcaaf1
Lőrik Levente <levminer@levminer.com>
2023-12-29 23:56:07 +0100

    footer

---
+++ apps/web/src/components/footer.svelte
@@ -0,0 +1,11 @@
+<footer class="body-font bg-[#110A30] text-gray-400">
+	<div class="container mx-auto flex flex-col items-center p-10">
+		<a class="title-font flex items-center justify-center font-medium text-white">
+			<img class="h-12 w-12" src={"/favicon.ico"} alt="Cores logo" />
+			<span class="ml-5 text-xl">Cores</span>
+		</a>
+		<p class="mt-5 text-center text-xl text-gray-400">
+			Copyright © 2023 Cores - Created by <a href="https://www.levminer.com" target="_blank" rel="noreferrer">Levminer</a>
+		</p>
+	</div>
+</footer>
