814812a262538656a7413fc1fc293e32bbcff384
Lőrik Levente <levminer@levminer.com>
2023-12-31 19:16:04 +0100

    setup pwa

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,8 +1,25 @@
 import { sveltekit } from "@sveltejs/kit/vite"
 import { defineConfig } from "vite"
+import { SvelteKitPWA } from "@vite-pwa/sveltekit"
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			manifest: {
+				name: "Cores - Hardware Monitor",
+				short_name: "Cores",
+				description: "Modern hardware monitor for Windows.",
+				background_color: "#000000",
+				theme_color: "#000000",
+				start_url: "/home",
+				display: "standalone",
+				orientation: "portrait-primary",
+				lang: "en-US",
+				categories: ["utilities"],
+			},
+		}),
+	],
 	server: {
 		fs: {
 			allow: ["../.."],
