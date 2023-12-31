5017134e520838399335b8b981db4f77efd1992d
Lőrik Levente <levminer@levminer.com>
2023-12-31 18:08:01 +0100

    Move app layout code

--- apps/web/src/routes/(app)/+layout.svelte
+++ apps/web/src/routes/(app)/+layout.svelte
@@ -11,4 +11,68 @@
 	import Loading from "ui/navigation/loading.svelte"
 	import BuildNumber from "ui/navigation/buildNumber.svelte"
 	import Navigation from "ui/navigation/navigation.svelte"
+	import { onMount } from "svelte"
+	import { hardwareStatistics, setHardwareStatistics } from "ui/stores/hardwareStatistics"
+	import init, { WebRtcClient } from "../../../../../crates/client/pkg/lib.js"
+	import { settings } from "ui/stores/settings"
+	import { generateMinutesData, generateSecondsData } from "ui/utils/stats"
+
+	onMount(() => {
+		let client: WebRtcClient | undefined
+
+		init().then(() => {
+			if ($settings.connectionCode!.startsWith("crs_")) {
+				client = new WebRtcClient($settings.connectionCode!)
+			}
+		})
+
+		// 60s date comparison
+		const date = new Date()
+		date.setSeconds(date.getSeconds() + 60)
+
+		// Update hardware statistics
+		const updateHardwareStats = (input: HardwareInfo) => {
+			if (Object.keys(input).length !== 0) {
+				if ($hardwareStatistics.minutes.length > 60) {
+					$hardwareStatistics.minutes.shift()
+				}
+
+				if ($hardwareStatistics.seconds.length > 60) {
+					$hardwareStatistics.seconds.shift()
+				}
+
+				let secondsData: Stats = generateSecondsData(input)
+
+				if (date.getTime() < new Date().getTime()) {
+					let minutesData: Stats = generateMinutesData(input, $hardwareStatistics)
+
+					// Update 60s timer
+					date.setSeconds(date.getSeconds() + 60)
+
+					setHardwareStatistics({
+						seconds: [...$hardwareStatistics.seconds, secondsData],
+						minutes: [...$hardwareStatistics.minutes, minutesData],
+					})
+				} else {
+					setHardwareStatistics({
+						seconds: [...$hardwareStatistics.seconds, secondsData],
+						minutes: [...$hardwareStatistics.minutes],
+					})
+				}
+			}
+		}
+
+		setInterval(() => {
+			if (client !== undefined) {
+				let message = client.get_message()
+
+				if (message !== undefined && message !== "") {
+					let parsed = JSON.parse(message)
+
+					setHardwareInfo(parsed)
+					updateHardwareStats(parsed)
+				}
+			}
+		}, 3000)
+	})
 </script>
--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -23,13 +23,7 @@
 
 <script lang="ts">
 	import "ui/styles/index.css"
-	import { onMount } from "svelte"
-	import { hardwareStatistics, setHardwareStatistics } from "ui/stores/hardwareStatistics"
-	import init, { WebRtcClient } from "../../../../crates/client/pkg/lib.js"
-	import { settings } from "ui/stores/settings"
-	import { setHardwareInfo } from "ui/stores/hardwareInfo"
 	import { onNavigate } from "$app/navigation"
-	import { generateMinutesData, generateSecondsData } from "ui/utils/stats"
 
 	onNavigate((navigation) => {
 		document.querySelector(".top")!.scrollIntoView()
@@ -42,64 +36,5 @@
 				await navigation.complete
 			})
 		})
-	})
-
-	onMount(() => {
-		let client: WebRtcClient | undefined
-
-		init().then(() => {
-			if ($settings.connectionCode!.startsWith("crs_")) {
-				client = new WebRtcClient($settings.connectionCode!)
-			}
-		})
-
-		// 60s date comparison
-		const date = new Date()
-		date.setSeconds(date.getSeconds() + 60)
-
-		// Update hardware statistics
-		const updateHardwareStats = (input: HardwareInfo) => {
-			if (Object.keys(input).length !== 0) {
-				if ($hardwareStatistics.minutes.length > 60) {
-					$hardwareStatistics.minutes.shift()
-				}
-
-				if ($hardwareStatistics.seconds.length > 60) {
-					$hardwareStatistics.seconds.shift()
-				}
-
-				let secondsData: Stats = generateSecondsData(input)
-
-				if (date.getTime() < new Date().getTime()) {
-					let minutesData: Stats = generateMinutesData(input, $hardwareStatistics)
-
-					// Update 60s timer
-					date.setSeconds(date.getSeconds() + 60)
-
-					setHardwareStatistics({
-						seconds: [...$hardwareStatistics.seconds, secondsData],
-						minutes: [...$hardwareStatistics.minutes, minutesData],
-					})
-				} else {
-					setHardwareStatistics({
-						seconds: [...$hardwareStatistics.seconds, secondsData],
-						minutes: [...$hardwareStatistics.minutes],
-					})
-				}
-			}
-		}
-
-		setInterval(() => {
-			if (client !== undefined) {
-				let message = client.get_message()
-
-				if (message !== undefined && message !== "") {
-					let parsed = JSON.parse(message)
-
-					setHardwareInfo(parsed)
-					updateHardwareStats(parsed)
-				}
-			}
-		}, 3000)
 	})
 </script>
