c68598b1784be649bb0ac87b65704326c0df872b
Lőrik Levente <levminer@levminer.com>
2024-02-27 08:42:35 +0100

    Update webmanifest

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
 	plugins: [
 		sveltekit(),
 		SvelteKitPWA({
+			registerType: "autoUpdate",
 			manifest: {
 				name: "Cores - Hardware Monitor",
 				short_name: "Cores",
@@ -17,18 +18,25 @@ export default defineConfig({
 				orientation: "portrait-primary",
 				lang: "en-US",
 				categories: ["utilities"],
+				dir: "ltr",
 				icons: [
 					{
 						src: "maskable_icon_x512.png",
 						sizes: "512x512",
 						type: "image/png",
-						purpose: 'any maskable'
+						purpose: "any maskable",
+					},
+					{
+						src: "maskable_icon_x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any",
 					},
 					{
 						src: "maskable_icon_x192.png",
 						sizes: "192x192",
 						type: "image/png",
-						purpose: 'any maskable'
+						purpose: "any maskable",
 					},
 				],
 			},
