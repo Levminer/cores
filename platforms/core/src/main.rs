c6eed0d91330f427af1366fa8058ea928582dee2
Lőrik Levente <levminer@levminer.com>
2024-04-27 18:03:18 +0200

    Tray icon

--- platforms/core/src/main.rs
+++ platforms/core/src/main.rs
@@ -1,16 +1,70 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::{
+    image::Image,
+    menu::{MenuBuilder, MenuItemBuilder},
+    tray::{ClickType, TrayIconBuilder},
+    Manager,
+};
+
+pub mod settings;
 
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_single_instance::init(|app, args, cwd| {
+            app.get_webview_window("main").unwrap().set_focus().unwrap();
+        }))
+        .invoke_handler(tauri::generate_handler![
+            settings::get_settings,
+            settings::set_settings
+        ])
+        .setup(|app| {
+            let toggle_window_item =
+                MenuItemBuilder::with_id("toggle_windows", "Show/Hide Cores").build(app)?;
+            let exit_item = MenuItemBuilder::with_id("exit", "Exit").build(app)?;
+            let menu = MenuBuilder::new(app)
+                .items(&[&toggle_window_item, &exit_item])
+                .build()?;
+
+            let _tray = TrayIconBuilder::new()
+                .icon(Image::from_path("icons/32x32.png").unwrap())
+                .tooltip("Cores")
+                .menu(&menu)
+                .on_menu_event(move |app, event| match event.id().as_ref() {
+                    "toggle_windows" => {
+                        let window = app.get_webview_window("main").unwrap();
+
+                        if window.is_visible().unwrap() {
+                            window.hide().unwrap();
+                        } else {
+                            window.show().unwrap();
+                            window.set_focus().unwrap();
+                        }
+                    }
+                    "exit" => {
+                        app.exit(0);
+                    }
+                    _ => (),
+                })
+                .on_tray_icon_event(|tray, event| {
+                    if event.click_type == ClickType::Left {
+                        let app = tray.app_handle();
+                        let window = app.get_webview_window("main").unwrap();
+
+                        if window.is_visible().unwrap() {
+                            window.hide().unwrap();
+                        } else {
+                            window.show().unwrap();
+                            window.set_focus().unwrap();
+                        }
+                    }
+                })
+                .build(app);
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
