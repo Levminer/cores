bc5caf20198e0dbe79b3f72b7c05da2893930971
Lőrik Levente <33373714+Levminer@users.noreply.github.com>
2025-04-04 22:09:30 +0200

    Updates (#37)
    Update prettier
    Fix GHSA-c9pr-q8gx-3mgp
    Update actions and rename

--- scripts/rename.js
+++ scripts/rename.js
@@ -1,6 +1,6 @@
-import { mkdirSync, existsSync, copyFileSync } from "fs"
+import { renameSync, existsSync, mkdirSync } from "fs"
 import { platform } from "os"
-import json from "../package.json" assert { type: "json" }
+import json from "../package.json" with { type: "json" }
 
 const os = platform()
 const version = json.version
@@ -11,25 +11,27 @@ if (!existsSync("./target/release/upload")) {
 
 if (os === "win32") {
 	try {
-		copyFileSync(`./target/release/bundle/msi/Cores_${version}_x64_en-US.msi`, `./target/release/upload/cores-${version}-windows-x64.msi`)
-		copyFileSync(`./target/release/bundle/msi/Cores_${version}_x64_en-US.msi.zip`, `./target/release/upload/cores-${version}-windows-x64.zip`)
-		copyFileSync(`./target/release/bundle/msi/Cores_${version}_x64_en-US.msi.zip.sig`, `./target/release/upload/cores-${version}-windows-x64.sig`)
+		renameSync(`./target/release/bundle/msi/Cores_${version}_x64_en-US.msi`, `./target/release/upload/cores-${version}-windows-x64.msi`)
+		renameSync(`./target/release/bundle/msi/Cores_${version}_x64_en-US.msi.zip`, `./target/release/upload/cores-${version}-windows-x64.zip`)
+		renameSync(`./target/release/bundle/msi/Cores_${version}_x64_en-US.msi.zip.sig`, `./target/release/upload/cores-${version}-windows-x64.sig`)
 	} catch (err) {
 		console.log("File not found")
 	}
 } else if (os === "darwin") {
 	try {
-		copyFileSync(`./target/release/bundle/dmg/Cores_${version}_aarch64.dmg`, `./target/release/upload/cores-${version}-macos-arm64.dmg`)
-		copyFileSync("./target/release/bundle/macos/Cores.app.tar.gz", `./target/release/upload/cores-${version}-macos-arm64.tar.gz`)
-		copyFileSync("./target/release/bundle/macos/Cores.app.tar.gz.sig", `./target/release/upload/cores-${version}-macos-arm64.sig`)
+		renameSync(`./target/release/bundle/dmg/Cores_${version}_aarch64.dmg`, `./target/release/upload/cores-${version}-macos-arm64.dmg`)
+		renameSync("./target/release/bundle/macos/Cores.app.tar.gz", `./target/release/upload/cores-${version}-macos-arm64.tar.gz`)
+		renameSync("./target/release/bundle/macos/Cores.app.tar.gz.sig", `./target/release/upload/cores-${version}-macos-arm64.sig`)
 	} catch (err) {
 		console.log("File not found", err)
 	}
 } else {
 	try {
-		copyFileSync(`./target/release/bundle/appimage/Cores_${version}_amd64.AppImage`, `./target/release/upload/cores-${version}-linux-x64.appimage`)
-		copyFileSync(`./target/release/bundle/deb/Cores_${version}_amd64.deb`, `./target/release/upload/cores-${version}-linux-x64.deb`)
-        copyFileSync(`./target/release/bundle/rpm/Cores-${version}-1.x86_64.rpm`, `./target/release/upload/cores-${version}-linux-x64.rpm`)
+		renameSync(`./target/release/bundle/appimage/Cores_${version}_amd64.AppImage`, `./target/release/upload/cores-${version}-linux-x64.appimage`)
+		renameSync(`./target/release/bundle/deb/Cores_${version}_amd64.deb`, `./target/release/upload/cores-${version}-linux-x64.deb`)
+		renameSync(`./target/release/bundle/rpm/Cores-${version}-1.x86_64.rpm`, `./target/release/upload/cores-${version}-linux-x64.rpm`)
+		renameSync(`./target/release/coresd`, `./target/release/upload/coresd-${version}-linux-x64`)
+		renameSync(`./target/aarch64-unknown-linux-gnu/release/coresd`, `./target/release/upload/coresd-${version}-linux-arm64`)
 	} catch (err) {
 		console.log("File not found")
 	}
