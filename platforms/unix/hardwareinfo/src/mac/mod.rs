8804f6497dda3b7d15ca69a64adf19fd42436190
Lőrik Levente <levminer@levminer.com>
2025-05-31 22:53:25 +0200

    fix mac gpu info

--- platforms/unix/hardwareinfo/src/mac/mod.rs
+++ platforms/unix/hardwareinfo/src/mac/mod.rs
@@ -11,7 +11,7 @@ pub fn macos_hardware_info(data: &mut Data) {
     use std::{process::Command, time::SystemTime};
     use sysinfo::Disks;
 
-    use crate::SmartctlDiskInfo;
+    use crate::{CoresGPUCard, SmartctlDiskInfo};
 
     let mut sampler = metrics::Sampler::new().unwrap();
     let soc = sources::SocInfo::new().unwrap();
@@ -21,24 +21,25 @@ pub fn macos_hardware_info(data: &mut Data) {
     if data.first_run {
         data.hw_info.cpu.info[0].manufacturer_name = "Apple".to_string();
         data.hw_info.cpu.info[0].socket_designation = soc.mac_model.clone();
-        data.hw_info.gpu.name = soc.chip_name.clone();
+        data.hw_info.gpu.cards.push(CoresGPUCard::default());
+        data.hw_info.gpu.cards[0].name = soc.chip_name.clone();
         data.hw_info.system.motherboard.name = soc.mac_model.clone();
 
-        data.hw_info.gpu.temperature.push(CoresSensor {
+        data.hw_info.gpu.cards[0].temperature.push(CoresSensor {
             name: "SOC".to_string(),
             value: (metrics.temp.gpu_temp_avg as f64).fmt_num(),
             min: (metrics.temp.gpu_temp_avg as f64).fmt_num(),
             max: (metrics.temp.gpu_temp_avg as f64).fmt_num(),
         });
 
-        data.hw_info.gpu.power.push(CoresSensor {
+        data.hw_info.gpu.cards[0].power.push(CoresSensor {
             name: "SOC".to_string(),
             value: (metrics.gpu_power as f64).fmt_num(),
             min: (metrics.gpu_power as f64).fmt_num(),
             max: (metrics.gpu_power as f64).fmt_num(),
         });
 
-        data.hw_info.gpu.clock.push(CoresSensor {
+        data.hw_info.gpu.cards[0].clock.push(CoresSensor {
             name: "SOC".to_string(),
             value: (metrics.gpu_usage.0 as f64).fmt_num(),
             min: (metrics.gpu_usage.0 as f64).fmt_num(),
@@ -59,14 +60,14 @@ pub fn macos_hardware_info(data: &mut Data) {
             max: (metrics.cpu_power as f64).fmt_num(),
         });
 
-        data.hw_info.gpu.load.push(CoresSensor {
+        data.hw_info.gpu.cards[0].load.push(CoresSensor {
             name: "Load".to_string(),
             value: (metrics.gpu_usage.1 as f64 * 100.0).fmt_num(),
             min: (metrics.gpu_usage.1 as f64 * 100.0).fmt_num(),
             max: (metrics.gpu_usage.1 as f64 * 100.0).fmt_num(),
         });
 
-        data.hw_info.gpu.max_load = (data.hw_info.gpu.load[0].value as f64).fmt_num();
+        data.hw_info.gpu.cards[0].max_load = (data.hw_info.gpu.cards[0].load[0].value as f64).fmt_num();
 
         // Disks
         let gb = 1024_f64.powi(3);
@@ -126,27 +127,27 @@ pub fn macos_hardware_info(data: &mut Data) {
             }
         }
     } else {
-        let prev_gpu_temp = data.hw_info.gpu.temperature[0].clone();
-        let prev_gpu_power = data.hw_info.gpu.power[0].clone();
+        let prev_gpu_temp = data.hw_info.gpu.cards[0].temperature[0].clone();
+        let prev_gpu_power = data.hw_info.gpu.cards[0].power[0].clone();
         let prev_cpu_temp = data.hw_info.cpu.temperature[0].clone();
         let prev_cpu_power = data.hw_info.cpu.power[0].clone();
-        let prev_gpu_load = data.hw_info.gpu.load[0].clone();
-        let prev_gpu_clock = data.hw_info.gpu.clock[0].clone();
+        let prev_gpu_load = data.hw_info.gpu.cards[0].load[0].clone();
+        let prev_gpu_clock = data.hw_info.gpu.cards[0].clock[0].clone();
 
-        data.hw_info.gpu.temperature[0] =
+        data.hw_info.gpu.cards[0].temperature[0] =
             compare_sensor(&prev_gpu_temp, (metrics.temp.gpu_temp_avg as f64).fmt_num());
-        data.hw_info.gpu.power[0] =
+        data.hw_info.gpu.cards[0].power[0] =
             compare_sensor(&prev_gpu_power, (metrics.gpu_power as f64).fmt_num());
         data.hw_info.cpu.temperature[0] =
             compare_sensor(&prev_cpu_temp, (metrics.temp.cpu_temp_avg as f64).fmt_num());
         data.hw_info.cpu.power[0] =
             compare_sensor(&prev_cpu_power, (metrics.cpu_power as f64).fmt_num());
-        data.hw_info.gpu.load[0] = compare_sensor(
+        data.hw_info.gpu.cards[0].load[0] = compare_sensor(
             &prev_gpu_load,
             (metrics.gpu_usage.1 as f64 * 100.0).fmt_num(),
         );
-        data.hw_info.gpu.max_load = (data.hw_info.gpu.load[0].value as f64).fmt_num();
-        data.hw_info.gpu.clock[0] =
+        data.hw_info.gpu.cards[0].max_load = (data.hw_info.gpu.cards[0].load[0].value as f64).fmt_num();
+        data.hw_info.gpu.cards[0].clock[0] =
             compare_sensor(&prev_gpu_clock, (metrics.gpu_usage.0 as f64).fmt_num());
     }
 }
