d4b0fea5e8383468b13b1fa9107c360d31a44698
Lőrik Levente <33373714+Levminer@users.noreply.github.com>
2025-08-16 23:09:24 +0200

    Update supabase.ts

--- platforms/interface/ui/utils/supabase.ts
+++ platforms/interface/ui/utils/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js"
 import type { Database } from "./database.d.ts"
 
 const PUBLIC_SUPABASE_ANON_KEY =
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpbG5sc2xocGdlZHRsYnhma2N6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUzMzQ2NjMsImV4cCI6MjA1MDkxMDY2M30.5P1xGPLsk-60AA-YG1aGHA53PQ_Lo8x_Gr_MzO38liY"
+	"sb_publishable_Zi57ivjBxc5SSqaEESn0Gg_qfhNTYIT"
 const PUBLIC_SUPABASE_URL = "https://ailnlslhpgedtlbxfkcz.supabase.co"
 
 export const supabaseClient = createClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
