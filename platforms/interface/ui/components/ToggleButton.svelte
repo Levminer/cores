8e23e116c052f1a1c61d72d7b3e57148f973b015
Lőrik Levente <33373714+Levminer@users.noreply.github.com>
2025-11-09 14:23:52 +0100

    fix

--- platforms/interface/ui/components/ToggleButton.svelte
+++ platforms/interface/ui/components/ToggleButton.svelte
@@ -1,4 +1,4 @@
-<button on:click class="mx-auto flex flex-1">
+<button {onclick} class="mx-auto flex flex-1">
 	<span
 		class="transparent-800 group m-1 inline-flex flex-shrink-0 flex-grow flex-col items-center justify-center rounded-lg p-1 px-3 text-gray-200 duration-200 ease-in-out"
 	>
@@ -7,5 +7,5 @@
 </button>
 
 <script lang="ts">
-	export let selected = false
+	let { selected = false, onclick }: { selected: boolean; onclick: () => void } = $props()
 </script>
