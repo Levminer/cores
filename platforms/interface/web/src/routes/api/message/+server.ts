d9053842b16aac29ca4552871c95000a653255b5
Lőrik Levente <33373714+Levminer@users.noreply.github.com>
2026-01-06 15:06:11 +0100

    bump version

--- platforms/interface/web/src/routes/api/message/+server.ts
+++ platforms/interface/web/src/routes/api/message/+server.ts
@@ -4,10 +4,10 @@ import type { RequestHandler } from "./$types"
 export const GET: RequestHandler = async ({ url }) => {
 	return Response.json(
 		{
-			title: "Cores 0.36.0 Updates",
-			message: "Improved support for Gigabyte motherboards, improved disk information and many more! Click show more to see all changes.",
-			link: "https://github.com/Levminer/cores/releases/tag/0.35.0",
-			date: "2025-11-22T15:41:14.481Z",
+			title: "Cores January 2026 Update",
+			message: "Modular home screen layout, Android app and more! Click show more to see all changes.",
+			link: "https://github.com/Levminer/cores/releases/tag/0.37.0",
+			date: "2026-01-06T15:41:14.481Z",
 		},
 		{ status: 200 },
 	)
