c3c68603f09ef5d34df93e14b2e77e0efcf2fbe7
Lőrik Levente <33373714+Levminer@users.noreply.github.com>
2026-01-13 12:01:21 +0100

    update LHML

--- platforms/windows/service/Program.cs
+++ platforms/windows/service/Program.cs
@@ -28,7 +28,7 @@ public class Program {
 	internal static Database Database = new();
 
 	private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e) {
-		Log.Error("App crashed with an unhandled exception");
+		Log.Error("App crashed with an unhandled exception: {@error}", e);
 		SentrySdk.CaptureException((Exception)e.ExceptionObject);
 	}
 
