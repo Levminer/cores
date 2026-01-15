feb64a43efb4bced3366d1ea1ca084a627c97140
Lőrik Levente <33373714+Levminer@users.noreply.github.com>
2026-01-15 15:00:39 +0100

    fix nvme temperatures

--- platforms/windows/lib/HardwareInfo.cs
+++ platforms/windows/lib/HardwareInfo.cs
@@ -519,7 +519,7 @@ public class HardwareInfo {
 
 					for (int j = 0; j < hardware.Sensors.Length; j++) {
 						// Drive temperature
-						if (sensor[j].SensorType == SensorType.Temperature) {
+						if (sensor[j].SensorType == SensorType.Temperature && !sensor[j].Name.Contains("warning") && !sensor[j].Name.Contains("critical")) {
 							// find disk by id and overwrite value
 							for (int k = 0; k < API.System.Storage.Disks.Count; k++) {
 								if (API.System.Storage.Disks[k].Id == computerHardware[i].Identifier.ToString()) {
