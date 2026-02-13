c312a7bf5925267d1acbdfa5a5a158e5185ddc15
Lőrik Levente <levminer@levminer.com>
2026-02-13 23:56:16 +0100

    Update message

--- platforms/interface/web/src/routes/api/message/+server.ts
+++ platforms/interface/web/src/routes/api/message/+server.ts
@@ -7,9 +7,9 @@ export const GET: RequestHandler = async ({ url, request }) => {
 
 	return Response.json(
 		{
-			title: "Cores January 2026 Update",
-			message: "Modular home screen layout, Android app and more! Click show more to see all changes.",
-			link: "https://github.com/Levminer/cores/releases/tag/0.37.0",
+			title: "Cores February 2026 Update",
+			message: "Improved support for Intel ARC and many bug fixes! Click show more to see all changes.",
+			link: "https://github.com/Levminer/cores/releases/tag/0.38.0",
 			date: "2026-01-06T15:41:14.481Z",
 		},
 		{ status: 200, headers },
