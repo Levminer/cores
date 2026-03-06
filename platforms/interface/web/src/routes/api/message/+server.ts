cdff01c660028a5568c00ff9831d67b8e286b5ef
Lőrik Levente <33373714+Levminer@users.noreply.github.com>
2026-03-06 19:19:56 +0100

    update message

--- platforms/interface/web/src/routes/api/message/+server.ts
+++ platforms/interface/web/src/routes/api/message/+server.ts
@@ -7,9 +7,9 @@ export const GET: RequestHandler = async ({ url, request }) => {
 
 	return Response.json(
 		{
-			title: "Cores February 2026 Update",
-			message: "Improved support for Intel ARC and many bug fixes! Click show more to see all changes.",
-			link: "https://github.com/Levminer/cores/releases/tag/0.38.0",
+			title: "Cores March 2026 Update",
+			message: "Disk usage improvements and better Anti-cheat compatibility. Click show more to see all changes.",
+			link: "https://github.com/Levminer/cores/releases/tag/0.39.0",
 			date: "2026-01-06T15:41:14.481Z",
 		},
 		{ status: 200, headers },
