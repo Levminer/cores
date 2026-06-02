90e8ea7e1c4fc731a6b54fdf1ff5e5c31bf74f31
Lőrik Levente <33373714+Levminer@users.noreply.github.com>
2026-06-02 15:19:05 +0200

    update release notes

--- platforms/interface/web/src/routes/api/message/+server.ts
+++ platforms/interface/web/src/routes/api/message/+server.ts
@@ -1,4 +1,3 @@
-import { redirect, json } from "@sveltejs/kit"
 import type { RequestHandler } from "./$types"
 
 export const GET: RequestHandler = async ({ url, request }) => {
@@ -7,9 +6,9 @@ export const GET: RequestHandler = async ({ url, request }) => {
 
 	return Response.json(
 		{
-			title: "Cores April 2026 Update",
-			message: "Edit home page layout on web/mobile and various bug fixes. Click show more to see all changes.",
-			link: "https://github.com/Levminer/cores/releases/tag/0.40.0",
+			title: "Cores June 2026 Update",
+			message: "Hide tiles on the home screen, notifications about hardware events and more. Click show more to see all changes.",
+			link: "https://github.com/Levminer/cores/releases/tag/0.41.0",
 			date: "2026-04-11T15:41:14.481Z",
 		},
 		{ status: 200, headers },
