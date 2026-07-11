01343fa08ce6adec3b77d7f192b1d748af174244
Lőrik Levente <33373714+Levminer@users.noreply.github.com>
2026-07-11 19:51:09 +0200

    Update release notes

--- platforms/interface/web/src/routes/api/message/+server.ts
+++ platforms/interface/web/src/routes/api/message/+server.ts
@@ -6,10 +6,10 @@ export const GET: RequestHandler = async ({ url, request }) => {
 
 	return Response.json(
 		{
-			title: "Cores June 2026 Update",
-			message: "Hide tiles on the home screen, notifications about hardware events and more. Click show more to see all changes.",
-			link: "https://github.com/Levminer/cores/releases/tag/0.41.0",
-			date: "2026-04-11T15:41:14.481Z",
+			title: "Cores July 2026 Update",
+			message: "Improvements on macOS and better disk usage on Windows. Click show more to see all changes.",
+			link: "https://github.com/Levminer/cores/releases/tag/0.42.0",
+			date: "2026-07-11T15:41:14.481Z",
 		},
 		{ status: 200, headers },
 	)
